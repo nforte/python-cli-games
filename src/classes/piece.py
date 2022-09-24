56924cdeb8e856e1b0da60bf73f4925c15f8b92f
nforte <noel.a.forte@gmail.com>
2022-09-24 16:23:09 -0700

    Added Card class

--- src/classes/piece.py
+++ src/classes/piece.py
@@ -11,3 +11,26 @@ class Piece:
         if other:
             return self.color == other.color
         return False #for handling None pieces
+
+class Card:
+    def __init__(self, value, suit='', backside='?'):
+        self.face = value + suit
+        self.backside = backside
+        self.isFaceDown = True
+
+        self.value = value
+        self.suit = suit
+
+    def __str__(self):
+        if self.isFaceDown:
+            return self.backside
+        else:
+            return self.face
+
+    def __eq__(self, other):
+        if other:
+            return self.face == self.face
+        return False
+
+    def flip(self):
+        self.isFaceDown = not self.isFaceDown
